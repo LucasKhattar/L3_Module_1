b54c76c35cd670811b752cae53359c770e2e83a6
ShadowWolf <league@WTS15.attlocal.net>
2017-10-15 17:28:16 -0700

    SearchingAndSortingAlgorithms(IP)
    I learned how to do this… ÷≥≤πßå∂˙¨ˆ∂√ç¡™¢∞¶•ª¶∑ªº–∑‘“«««««≠–…
    Your welcome

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,20 +1,70 @@
 package SearchingAndSortingAlgorithms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		for(int i = 0; i < eggs.size(); i++){
+		for (int i = 0; i < eggs.size(); i++) {
 			String crack = eggs.get(i);
-			if(crack.equals("cracked")) {
-				
+			if (crack.equals("cracked")) {
 				return i;
 			}
 		}
-			
+		return 0;
+	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		for (int i = 0; i < oysters.size(); i++) {
+			Boolean pearl = oysters.get(i);
+			if (pearl.equals(true)) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	public static Double findTallest(List<Double> peeps) {
+		Double big = peeps.get(0);
+		for (int i = 1; i < peeps.size(); i++) {
+			if (big < peeps.get(i)) {
+				big = peeps.get(i);
+			}
+		}
+		return big;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String big = words.get(0);
+		for (int i = 1; i < words.size(); i++) {
+			if (big.length() < words.get(i).length()) {
+				big = words.get(i);
+			}
+		}
+		return big;
+	}
+
+	public static Boolean containsSOS(List<String> morse) {
+		if (morse.contains(" ... --- ... ")) {
+			return true;
+		}
+		else{
+			return false;	
+		}
+	}
+
+	public static List<Double> sortScores(List<Double> results) {
+		Collections.sort(results);
+		return results;
+	}
+
+	public static List<String> sortDNA(List<String> unsortedSequences){
 		
-		return 0; //<- this needs changing
 	}
 	
-	//Add other methods here
+	public static List<String> sortWords(List<String> words){
+		Collections.sort(words);
+		return words;
+	}
+	// Add other methods here
 }
